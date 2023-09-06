3735c992926b10de5426edc4497ea69aec992c69
pryority <matthewapryor@gmail.com>
2023-09-06 18:05:23 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -71,36 +71,3 @@ struct Product {
     pub price: u32,
     pub sale_price: u32,
 }
-
-// async fn create(product: &Product, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
-//     let query =
-//         "INSERT INTO product (title, description, sku, quantity, price, sale_price) VALUES ($1, $2, $3, $4, $5, $6)";
-
-//     sqlx::query(query)
-//         .bind(&product.title)
-//         .bind(&product.description)
-//         .bind(&product.sku)
-//         .bind(product.quantity as i32)
-//         .bind(product.price as i32) // Convert u32 to i32
-//         .bind(product.sale_price as i32) // Convert u32 to i32
-//         .execute(pool)
-//         .await?;
-
-//     Ok(())
-// }
-
-// async fn update(product: &Product, sku: &str, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
-//     let query = "UPDATE product SET title = $1, description = $2, sku = $3, quantity = $4, price = $5, sale_price = $6 WHERE sku = $3";
-
-//     sqlx::query(query)
-//         .bind(&product.title)
-//         .bind(&product.description)
-//         .bind(&product.sku)
-//         .bind(product.quantity as i32)
-//         .bind(product.price as i32)
-//         .bind(product.sale_price as i32)
-//         .execute(pool)
-//         .await?;
-
-//     Ok(())
-// }
