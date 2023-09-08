91afad99469d385898040ba9448a4242f531fd8d
pryority <matthewapryor@gmail.com>
2023-09-07 20:55:21 -0400

    update ProductModel schemas

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -19,9 +19,11 @@ pub struct CreateProductSchema {
     pub title: String,
     pub description: String,
     pub sku: u32,
+    pub category: String,
     pub quantity: u32,
     pub price: u32,
     pub sale_price: u32,
+    pub on_sale: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -29,7 +31,9 @@ pub struct UpdateProductSchema {
     pub title: Option<String>,
     pub description: Option<String>,
     pub sku: Option<u32>,
+    pub category: Option<String>,
     pub quantity: Option<u32>,
     pub price: Option<u32>,
     pub sale_price: Option<u32>,
+    pub on_sale: Option<bool>,
 }
